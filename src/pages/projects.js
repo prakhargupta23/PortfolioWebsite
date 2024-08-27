18e39840af7cf795c02f3097e382eb70963d7f4e
Prakhar Gupta <prakhar2332@gmail.com>
2024-08-27 23:19:24 +0530

    change commit

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -82,44 +82,17 @@ const projects = () => {
             <AnimatedText text="Featured Projects" className='mb-16'/>
             <div className='grid grid-cols-12 gap-24 gap-y-32'>
                 <div className='col-span-12'>
-                <FeaturedProjects title="Image to Excel App" summary="The Image to Excel App simplifies grading by allowing users to capture photos of answer sheets. The app automatically extracts question-wise marks and inserts them into an Excel sheet. Utilizing Flutter for the front-end and Python with OpenCV for image processing, it reduces manual data entry, minimizes errors, and accelerates the grading process." link="/" type="Professional Work" github="https://github.com/prakhargupta23/Image-to-Exel-App" img={p1}/>
+                <FeaturedProjects title="Image to Excel App" summary="The Image to Excel App simplifies grading by allowing users to capture photos of answer sheets. The app automatically extracts question-wise marks and inserts them into an Excel sheet. Utilizing Flutter for the front-end and Python with OpenCV for image processing, it reduces manual data entry, minimizes errors, and accelerates the grading process." link="https://github.com/prakhargupta23/Image-to-Exel-App" type="Professional Work" github="https://github.com/prakhargupta23/Image-to-Exel-App" img={p1}/>
                 
             </div>
             
             <div className='col-span-6'>
-            <Project title="LNMIIT GuestHouse Booking" summary="A feature-rich Crypto Screener App using React, Tailwind CSS, Context API, React Router and Recharts. 
-It shows detail regarding almost all the cryptocurrency. You can easily convert the price in your 
-local currency." link="/" type="Professional Project" github="https://github.com/prakhargupta23/guesthouse" img={p3}/>
+            <Project title="GuestHouse Booking" summary="" link="https://github.com/prakhargupta23/guesthouse" type="Professional Project" github="https://github.com/prakhargupta23/guesthouse" img={p3}/>
             </div>
 
             <div className='col-span-6'>
-            <Project title="Portfolio Website" summary="A feature-rich Crypto Screener App using React, Tailwind CSS, Context API, React Router and Recharts. 
-It shows detail regarding almost all the cryptocurrency. You can easily convert the price in your 
-local currency." link="/" type="Personal Project" github="https://github.com/prakhargupta23/PortfolioWebsite" img={p2}/>
+            <Project title="Portfolio Website" summary="" link="/" type="Personal Project" github="https://github.com/prakhargupta23/PortfolioWebsite" img={p2}/>
             </div>
-
-            
-
-            
-                {/* <div className='col-span-12'>
-                <FeaturedProjects title="Crypto Screener Application" summary="A feature-rich Crypto Screener App using React, Tailwind CSS, Context API, React Router and Recharts. 
-It shows detail regarding almost all the cryptocurrency. You can easily convert the price in your 
-local currency." link="/" type="Featured Project" github="/" img={project1}/>
-                
-            </div>
-
-
-            <div className='col-span-6'>
-            <Project title="Crypto Screener Application" summary="A feature-rich Crypto Screener App using React, Tailwind CSS, Context API, React Router and Recharts. 
-It shows detail regarding almost all the cryptocurrency. You can easily convert the price in your 
-local currency." link="/" type="Featured Project" github="/" img={project1}/>
-            </div>
-
-            <div className='col-span-6'>
-            <Project title="Crypto Screener Application" summary="A feature-rich Crypto Screener App using React, Tailwind CSS, Context API, React Router and Recharts. 
-It shows detail regarding almost all the cryptocurrency. You can easily convert the price in your 
-local currency." link="/" type="Featured Project" github="/" img={project1}/>
-            </div> */}
 
             </div>
             
